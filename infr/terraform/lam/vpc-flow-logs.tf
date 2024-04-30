bfe36b88cd3e4ec3d0297241821917e2b9a82228
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-04-30 14:14:53 +0300

    change name vpc flow logs

--- infr/terraform/lam/vpc-flow-logs.tf
+++ infr/terraform/lam/vpc-flow-logs.tf
@@ -1,4 +1,4 @@
-resource "aws_flow_log" "example" {
+resource "aws_flow_log" "lam_logs" {
   iam_role_arn    = aws_iam_role.lam_role.arn
   log_destination = aws_cloudwatch_log_group.lam_cloudwatch.arn
   traffic_type    = "ALL"
